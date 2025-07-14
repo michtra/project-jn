5aaa0b08b53495fde81044ba33801e28a7317bfc
charles23smith <197137721+charles23smith@users.noreply.github.com>
2025-07-14 05:51:52 -0500

    Bug fixes for gauth/parseData and working data parser implementation

---
+++ gauth.js
@@ -0,0 +1,84 @@
+let currentUser = null;
+let token = null;
+let tokenClient = null;
+
+
+function handleCredentialResponse(response) {
+    try {
+        //get user info
+        const userinfo = decodeResponse(response.credential);
+
+        //define user data into current user
+        currentUser = {
+            id: userinfo.sub,
+            name: userinfo.name,
+            email: userinfo.email,
+            picture: userinfo.picture
+        };
+
+        updateUI();
+    }
+    catch (error) {
+        console.error("Error with credential reponse", error);
+    }
+
+}
+
+//decode JWT token and parse into JavaScript Object
+function decodeResponse(token) {
+    //make token presentable
+    const base64Url = token.split(".")[1];
+    const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+    const jsonPayload = decodeURIComponent(atob(base64).split("").map(function (c) {
+        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(""));
+    return JSON.parse(jsonPayload);
+}
+
+function getToken() {
+    //prompt user for verfication of data, then fetch token for API 
+    return new Promise((resolve, reject) => {
+
+        tokenClient = google.accounts.oauth2.initTokenClient({
+            client_id: "473070785433-j66nu8cvkos5mkhml3se4mr1b4iop3po.apps.googleusercontent.com",
+            scope: "https://www.googleapis.com/auth/spreadsheets.readonly https://www.googleapis.com/auth/drive.metadata.readonly",
+            callback: (tokenResponse) => {
+                if (tokenResponse.error) {
+                    reject(tokenResponse);
+                } else {
+                    token = tokenResponse.access_token;
+                    resolve(token);
+                }
+            }
+        });
+        tokenClient.requestAccessToken({ prompt: "" });
+    });
+}
+
+function updateUI() {
+    //get rid of sign in button and show credentials, as well as spreadsheet button 
+    if (currentUser) {
+        document.getElementById("signin").classList.add("hidden");
+        document.getElementById("user-info").classList.remove("hidden");
+        document.getElementById("sheets-selection").classList.remove("hidden");
+        document.getElementById("display-user-info").innerHTML = `
+            <img src = "${currentUser.picture}" alt = "User Profile Pic">
+            <div>
+                <a> 👋Hello , ${currentUser.name} (${currentUser.email}) ! </a>
+            </div>
+        ` ;
+    } else {
+        //restore defaults
+        document.getElementById("signin").classList.remove("hidden");
+        document.getElementById("user-info").classList.add("hidden");
+        document.getElementById("sheets-selection").classList.add("hidden");
+    }
+}
+
+//allow everything to load properly
+window.onload = function () {
+    setTimeout(() => {
+        updateUI();
+    }, 100);
+};
+
---
+++ parseData.py
@@ -0,0 +1,104 @@
+import copy
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask("__name__")
+CORS(app)
+
+#horizontal scanning function (assuming CLS style programming sheet; could account for more weeks)
+def horizontal_parse(jsondata):
+    data = jsondata['values']
+
+    #find if the program has an offset (blank space on left), delete (only checking if leftmost col is empty based on programs)
+    leftisBlank = True 
+    for row in data:
+        if len(row) > 0 and row[0] !=  "":
+            leftisBlank = False
+
+    if (leftisBlank):
+        for row in data:
+            if len(row) > 0:
+                del row[0]
+
+    #get the array consisting of the week names (Week1, Week2, etc. )
+    weeks = []
+    for row in data:
+        if len(row) > 0 and row[0].startswith("Week"):
+            weeks = row
+
+    #loop and populate new array with every week defined 
+    weekArray = []
+    for i in weeks:
+        if i.startswith("Week") or i.startswith("week"):
+            weekArray.append(i)
+
+    #create dictionary with inner dictionaries where keys = days of week and values = array of lifts needed to be performed
+    sheetDictionary = {key: {} for key in weekArray}
+    daysOfWeek = []
+
+    for row in data:
+        if len(row) > 0 and any(day in row[0] for day in ["Mon","Tues","Wed","Thur","Fri","Sat","Sun"]):
+            daysOfWeek.append(row[0])
+
+    innerDictionary = {key: {} for key in daysOfWeek}
+    numofWeeks = len(weekArray)
+    for i in range(numofWeeks):
+        sheetDictionary[weekArray[i]] = copy.deepcopy(innerDictionary)
+
+    #implement sorting loop to get final dictionary
+    weekstarted = False
+    currentDay = ""
+    
+    for row in data:
+        #find a header marking the day
+        if len(row) > 0 and row[0] in daysOfWeek:
+            weekstarted = True
+            currentDay = row[0]
+            continue
+
+        #skip until a header is found 
+        if not (weekstarted):
+            continue
+
+        #skip if not valid row
+        if len(row) == 0:
+            continue
+
+        #check for rest day
+        if row[0] == "rest" or row[0] == "Rest":
+            for i in range(numofWeeks):
+                exerciseDictionary = {"Rest": 0}
+                sheetDictionary[weekArray[i]][currentDay]["Rest"] = exerciseDictionary
+            continue
+        
+        if currentDay != row[0]:
+            for i in range(numofWeeks):
+                #these two are always defined
+                Exercise = row[0 + 5*i] if len(row) > 0 + 5*i else ""
+                Prescribed = row[1 + 5*i] if len(row) > 1 + 5*i else ""
+
+                #these need to be checked if there or not (jumps defined by spaces apart in google sheet following CLS format)
+                Weight = row[2 + 5*i] if len(row) > 2 + 5*i else ""
+                RPE = row[3 + 5*i] if len(row) > 3 + 5*i else ""
+                Notes = row[4 + 5*i] if len(row) > 4 + 5*i else ""
+    
+                exerciseDictionary = {"Prescribed": Prescribed, "Weight": Weight, "RPE": RPE, "Notes": Notes}
+                if Exercise in sheetDictionary[weekArray[i]][currentDay]:
+                    Exercise = f"{Exercise} (Backdown)"
+
+                sheetDictionary[weekArray[i]][currentDay][Exercise] = exerciseDictionary
+
+    return sheetDictionary
+
+@app.route("/data", methods = ["POST"])
+def getData():
+    data = request.get_json()
+    data_sorted = horizontal_parse(data)
+    return jsonify(data_sorted)
+
+if __name__ == "__main__":
+    app.run(port = 5000, debug = True)
+
+
+
+    
